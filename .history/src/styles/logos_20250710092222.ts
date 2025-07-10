e1357a6fcd52184c66b76b3e615f41dcabf0e1e0
RafaelMGaldino <101841163+RafaelMGaldino@users.noreply.github.com>
2025-07-10 09:31:07 -0300

    Feat: Form component

--- .history/src/styles/logos_20250710092222.ts
+++ .history/src/styles/logos_20250710092222.ts
@@ -1,6 +1,6 @@
 import "styled-components";
 
 export const logos: { black: string; white: string } = {
-    black: '/dnc-logo-black.svg',
-    white: '/dnc-logo-white.svg'
+    black: '../dnc-logo-black.svg',
+    white: '../dnc-logo-white.svg'
 };
