d6067db35e4bce0dada0b6952303722fb9e37866
RafaelMGaldino <101841163+RafaelMGaldino@users.noreply.github.com>
2025-07-28 09:47:51 -0300

    Feat: Adding Users in Card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,38 @@
-import { CardComponent, Header } from '@/components'
-import { Container } from '@mui/material'
+import { AvatarList, CardComponent, Header } from "@/components";
+import {Container} from '@mui/material'
+import { currencyConverter } from "@/utils";
+
 
 function Home() {
+  const monckListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1234.54),
+    },
+     {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle:  currencyConverter(4232.54),
+    },
+     {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(3242.54),
+    },
+  ]
   return (
     <>
-      <Header />
-      <Container maxWidth="lg">
-        <CardComponent className="alert">Card</CardComponent>
-      </Container>
-    </>
+       <Header/>
+       <Container maxWidth='lg'>
+        <CardComponent  className='primary'>Card</CardComponent>
+        <CardComponent>
+          <AvatarList listData={monckListData}/>
+        </CardComponent>
+
+       </Container>
+
+        </>
   )
 }
 export default Home
